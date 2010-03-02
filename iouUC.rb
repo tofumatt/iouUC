67c3378aa287c1efb269fe1ad7cee706e8050be4
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-03-02 19:43:16 -0400

    Formatting issue
    Spaces, not tabs! I seem to keep messing up my lang configs in TextMate.

--- iouUC.rb
+++ iouUC.rb
@@ -20,56 +20,56 @@ set :public, 'public'
 # Setup the app
 configure do
   environment = :development
-	
+  
   DB = Sequel.sqlite#('iouUC.sqlite')
   
-	# Database version 1
-	unless DB.table_exists? :schema
-		DB.create_table :schema do
-	    primary_key :version
-	    Time :date_created
-	  end
-		
-	  DB.create_table :people do
-	    primary_key :id
-	    String :name
-	    Time :date_created
-	  end
+  # Database version 1
+  unless DB.table_exists? :schema
+    DB.create_table :schema do
+      primary_key :version
+      Time :date_created
+    end
+    
+    DB.create_table :people do
+      primary_key :id
+      String :name
+      Time :date_created
+    end
   
-	  DB.create_table :products do
-	    primary_key :id
-			String :name
-	    Float :cost
-	    Time :date_created
-	  end
+    DB.create_table :products do
+      primary_key :id
+      String :name
+      Float :cost
+      Time :date_created
+    end
   
-	  DB.create_table :transactions do
-	    primary_key :id
-	    Float :amount
-	    Float :person_for # Linking relationship to a person's id
-	    Float :person_owed # Linking relationship to a person's id
-	    Time :date_created
-	  end
+    DB.create_table :transactions do
+      primary_key :id
+      Float :amount
+      Float :person_for # Linking relationship to a person's id
+      Float :person_owed # Linking relationship to a person's id
+      Time :date_created
+    end
   
-		if environment == :development
-	  	DB[:people].insert(:name => 'Matt', :date_created => Time.now)
-			DB[:people].insert(:name => 'Dave', :date_created => Time.now)
-		
-			DB[:products].insert(:name => 'Cookie', :cost => 2, :date_created => Time.now)
-		
-			for i in 1..2
-				DB[:transactions].insert(:amount => 2, :person_for => 2, :person_owed => 1, :date_created => Time.now)
-			end
-			DB[:transactions].insert(:amount => 2.50, :person_for => 2, :person_owed => 1, :date_created => Time.now)
-		end
-	end
+    if environment == :development
+      DB[:people].insert(:name => 'Matt', :date_created => Time.now)
+      DB[:people].insert(:name => 'Dave', :date_created => Time.now)
+    
+      DB[:products].insert(:name => 'Cookie', :cost => 2, :date_created => Time.now)
+    
+      for i in 1..2
+        DB[:transactions].insert(:amount => 2, :person_for => 2, :person_owed => 1, :date_created => Time.now)
+      end
+      DB[:transactions].insert(:amount => 2.50, :person_for => 2, :person_owed => 1, :date_created => Time.now)
+    end
+  end
 end
 
 # Homepage displays a list of all people and their current debt
 get '/' do
   @title = "Home"
-	@people = DB[:people].order(:name.asc)
-	@transactions = DB[:transactions].order(:date_created.desc).limit(5)
+  @people = DB[:people].order(:name.asc)
+  @transactions = DB[:transactions].order(:date_created.desc).limit(5)
   
   haml :index
 end
@@ -112,24 +112,24 @@ end
 
 # Format a Time object into a pretty date string
 def date_format(time)
-	time.strftime "%B %d (%A)"
+  time.strftime "%B %d (%A)"
 end
 
 # Get a total debt number for a person
 def debt(person_for, person_owed)
-	DB[:transactions].filter(:person_owed => person_owed[:id], :person_for => person_for[:id]).sum(:amount) || 0
+  DB[:transactions].filter(:person_owed => person_owed[:id], :person_for => person_for[:id]).sum(:amount) || 0
 end
 
 # Format a number into a pretty dollar string
 def money_format(amount)
-	amount = amount.to_s
-	split = amount.split('.', 2)
-	
-	if split[1].nil?
-		"$#{split[0]}.00"
-	elsif split[1].length == 1
-		"$#{split[0]}.#{split[1]}0"
-	else
-		"$#{split[0]}.#{split[1]}"
-	end
+  amount = amount.to_s
+  split = amount.split('.', 2)
+  
+  if split[1].nil?
+    "$#{split[0]}.00"
+  elsif split[1].length == 1
+    "$#{split[0]}.#{split[1]}0"
+  else
+    "$#{split[0]}.#{split[1]}"
+  end
 end

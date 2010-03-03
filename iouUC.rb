a7aa0852ed6f5719cb206e870605453b2ad1e42d
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-03-02 22:16:48 -0400

    App create
    So this at least kinda works. It's really pretty silly.

--- iouUC.rb
+++ iouUC.rb
@@ -19,9 +19,9 @@ set :public, 'public'
 
 # Setup the app
 configure do
-  environment = :development
+  environment = :production
   
-  DB = Sequel.sqlite#('iouUC.sqlite')
+  DB = Sequel.sqlite("iouUC.sqlite")
   
   # Database version 1
   unless DB.table_exists? :schema
@@ -50,28 +50,12 @@ configure do
       Float :person_owed # Linking relationship to a person's id
       Time :date_created
     end
-  
-    if environment == :development
-      DB[:people].insert(:name => 'Matt', :date_created => Time.now)
-      DB[:people].insert(:name => 'Dave', :date_created => Time.now)
-    
-      DB[:products].insert(:name => 'Cookie', :cost => 2, :date_created => Time.now)
-    
-      for i in 1..2
-        DB[:transactions].insert(:amount => 2, :person_for => 2, :person_owed => 1, :date_created => Time.now)
-      end
-      DB[:transactions].insert(:amount => 2.50, :person_for => 2, :person_owed => 1, :date_created => Time.now)
-    end
   end
 end
 
 # Homepage displays a list of all people and their current debt
 get '/' do
-  @title = "Home"
-  @people = DB[:people].order(:name.asc)
-  @transactions = DB[:transactions].order(:date_created.desc).limit(5)
-  
-  haml :index
+  index
 end
 
 # List the transactions for a specific person in reverse chronological order
@@ -86,22 +70,16 @@ end
 
 # Create/edit a person
 post '/people' do
-  if params[:people] and params[:people][:id]
-    # Edit a person with this id
-    DB[:people].update(params[:person].merge(:date_created => Time.now)).filter(:id => params[:person][:id])
-  else
-    # Create a new person
-    DB[:people].insert(params[:person].merge(:date_created => Time.now))
-  end
+  process_person
   
-  redirect '/'
+  index
 end
 
 # Add a transaction
 post '/transactions' do
-  DB[:transactions].insert(params[:transaction].merge(:date_created => Time.now))
+  process_transactions
   
-  redirect '/'
+  index
 end
 
 # Create css files from the sass files in our stylesheets directory
@@ -120,6 +98,14 @@ def debt(person_for, person_owed)
   DB[:transactions].filter(:person_owed => person_owed[:id], :person_for => person_for[:id]).sum(:amount) || 0
 end
 
+def index
+  @title = "Main Screen Turn On"
+  @people = DB[:people].order(:name.asc)
+  @transactions = DB[:transactions].order(:date_created.desc).limit(5)
+  
+  haml :index
+end
+
 # Format a number into a pretty dollar string
 def money_format(amount)
   amount = amount.to_s
@@ -131,5 +117,56 @@ def money_format(amount)
     "$#{split[0]}.#{split[1]}0"
   else
     "$#{split[0]}.#{split[1]}"
+  end
+end
+
+# Validate/process person data
+def process_person
+  if DB[:people].filter(:name => params[:person][:name]).count > 0
+    if @errors.nil?
+      @errors = []
+    end
+    @errors << "Someone with that name already exists."
+  elsif params[:person][:name].length < 1
+    if @errors.nil?
+      @errors = []
+    end
+    @errors << "You need to name the person you want to create."
+  end
+  
+  if @errors.nil?
+    # Create a new person
+    DB[:people].insert(params[:person].merge(:date_created => Time.now))
+    
+    redirect '/'
+  end
+end
+
+# Validate/process transaction data
+def process_transactions
+  amount = params[:transaction][:amount].to_s.split('.', 2)
+  unless amount[1].nil? or amount[1].length <= 2
+    params[:transaction][:amount] = (amount[0] + '.' + amount[1][0..1]).to_f
+  end
+  
+  if params[:transaction][:amount].to_f < 0.01
+    if @errors.nil?
+      @errors = []
+    end
+    @errors << "You have to owe at least a penny."
+  elsif params[:transaction][:person_for] == params[:transaction][:person_owed]
+    if @errors.nil?
+      @errors = []
+    end
+    @errors << "You can't owe yourself money!"
+  end
+  
+  if params[:operator] == "-"
+    params[:transaction][:amount] = -params[:transaction][:amount].to_f
+  end
+  
+  if @errors.nil?
+    DB[:transactions].insert(params[:transaction].merge(:date_created => Time.now))
+    redirect '/'
   end
 end
